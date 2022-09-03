18d67e8e903cca23cb70e2a7148025776e9ef91c
IagoCB <iago.luiz@ges.inatel.br>
2022-09-03 16:38:32 -0300

    :tada: Commit inicial

---
+++ src/infrastructure/database.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const uri = `mongodb+srv://user:user@labc214.ajb6f.mongodb.net/myFirstDatabase?`;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    id: {
+        type: String,
+        index: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    nome: String,
+    senha: String,
+});
+
+const UserModel = mongoose.model('UserModel', UserSchema);
+
+module.exports = {
+    UserModel,
+};
---
+++ src/port/user_api.js
@@ -0,0 +1,32 @@
+const User = require('../application/user_service');
+const Utils = require('../utils/utils');
+
+const route = '/user';
+
+module.exports = (app) => {
+    app.post(`${route}/create`, async (req, res) => {
+        const response = await User.create(req.body);
+        res.status(Utils.responseStatus(response.name));
+        res.json(response);
+    });
+    app.put(`${route}/update`, async (req, res) => {
+        const response = await User.update(req.body);
+        res.status(Utils.responseStatus(response.name));
+        res.json(response);
+    });
+    app.delete(`${route}/delete`, async (req, res) => {
+        const response = await User.delete(req.body);
+        res.status(Utils.responseStatus(response.name));
+        res.json(response);
+    });
+    app.get(`${route}/list`, async (req, res) => {
+        const response = await User.list();
+        res.status(Utils.responseStatus(response.name));
+        res.json(response);
+    });
+    app.patch(`${route}/listUser`, async (req, res) => {
+        const response = await User.listByEmail(req.body);
+        res.status(Utils.responseStatus(response.name));
+        res.json(response);
+    });
+};
