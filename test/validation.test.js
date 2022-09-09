4061558a247d37eedbf2bc761bf4ffaf4710c7a1
IagoCB <iago.luiz@ges.inatel.br>
2022-09-09 11:16:28 -0300

    :test_tube: Implementando teste unitário

---
+++ test/validation.test.js
@@ -0,0 +1,19 @@
+const Constants = require('../src/utils/Constants');
+const Validation = require('../src/utils/validation');
+
+it('Caso válido', () => {
+    const result = Validation.create({
+        nome: "Iago",
+        email: "iago.luiz@ges.inatel.br",
+        senha: "123456789"
+    });
+    expect(result).toEqual(undefined);
+});
+
+it('Caso inválido - sem o parâmetro nome', () => {
+    const result = Validation.create({
+        email: "iago.luiz@ges.inatel.br",
+        senha: "123456789"
+    });
+    expect(result.name).toEqual(Constants.ErrorValidation.name);
+});
